1891cf5ec3c458ffa256f96b84f4bed2bd99f8dd
Shahin-A <shahina@berkeley.edu>
2014-03-06 22:54:08 -0800

    first commit

---
+++ Zagros/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zagros::Application.config.secret_key_base = 'f06dc99516d8c1b6e57b1a0f0db6af06547306ee3f6a2b0a7c2b1362ac7f92bb568ca8c851a3ba491a14b9990107b97c56cc9984d884cdd30778ed5e26e91ecf'
